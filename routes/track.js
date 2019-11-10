ff8f68c809759ef4a92ca6e574dea9e15fae5520
Jakub Kopyto <j.kopyto@iambot.ai>
2019-11-10 18:57:50 +0100

    Missclick fixed

--- routes/track.js
+++ routes/track.js
@@ -9,7 +9,7 @@ router.get("/:id", [auth], async (req, res) => {
   const trackId = await Track.findById(req.params.id)
   if (!trackId) return res.status(400).send("There is no track with that id")
 
-  const filePath = `/public/tracks/${trackId.title}`
+  const filePath = `/public/tracks/${req.params.id}`
   res.download(filePath, trackId.title)
 })
 
