d40a3bd71e83599a7883a6dd381f33a724a6d8b3
Jakub Kopyto <j.kopyto@iambot.ai>
2019-11-10 18:20:16 +0100

    Rename function

--- routes/register.js
+++ routes/register.js
@@ -1,12 +1,12 @@
 const bcrypt = require("bcrypt")
 const _ = require("lodash")
-const { User, validate } = require("../models/user")
+const { User, validateUser } = require("../models/user")
 const express = require("express")
 
 const router = express.Router()
 
 router.post("/", async (req, res) => {
-  const { error } = validate(req.body)
+  const { error } = validateUser(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
   let user = await User.findOne({
