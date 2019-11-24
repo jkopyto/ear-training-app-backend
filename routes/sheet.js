27b5321d7305955b5914fe9ce06d4fb812c46923
Jakub Kopyto <j.kopyto@iambot.ai>
2019-11-24 14:11:10 +0100

    Tests

--- routes/sheet.js
+++ routes/sheet.js
@@ -26,6 +26,7 @@ router.post("/new", [auth], async (req, res) => {
   if (error.message) return res.status(400).send("Invalid body")
 
   const user = await User.findById(req.user_id)
+  console.log(user)
   if (!user || !user.addNewRes) return res.status(403).send("Forbidden")
 
   let sheet = await Sheet.findOne({
